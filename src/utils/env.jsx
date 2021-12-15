676de2a7deaa734d8141f394c2869daaf96037be
Alexis <alexis.prunier@securitymadein.lu>
2021-12-15 14:34:12 +0100

    Review env endpoints

--- src/utils/env.jsx
+++ src/utils/env.jsx
@@ -2,10 +2,7 @@ export function getApiURL() {
 	if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1" || window.location.hostname === "") {
 		return "http://localhost:5000/";
 	}
-	if (window.location.hostname.includes("test")) {
-		return "https://api.test-db.cy.lu/";
-	}
-	return "https://api.cybersecurity-luxembourg.com/";
+	return "https://api." + window.location.hostname.replace("www.", "").split(".").slice(1).join(".");
 }
 
 export function getGlobalAppURL() {
@@ -15,21 +12,18 @@ export function getGlobalAppURL() {
 	if (window.location.hostname.includes("test")) {
 		return "https://test.cy.lu/";
 	}
-	return "https://cybersecurity-luxembourg.com/";
+	return "https://" + window.location.hostname.replace("www.", "").split(".").slice(1).join(".");
 }
 
 export function getCookieOptions() {
-	// TODO use httponly cookies
 	if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1" || window.location.hostname === "") {
-		// return { httpOnly: true };
 		return { domain: "localhost", path: "/" };
 	}
-	if (window.location.hostname.includes("test")) {
-		// return { secure: true, domain: ".cy.lu" httpOnly: true };
-		return { secure: true, domain: ".cy.lu", path: "/" };
-	}
-	// return { secure: true, domain: ".cy.lu", httpOnly: true };
-	return { secure: true, domain: ".cybersecurity-luxembourg.com", path: "/" };
+	return {
+		secure: true,
+		domain: "." + window.location.hostname.replace("www.", "").split(".").slice(1).join("."),
+		path: "/",
+	};
 }
 
 export function isInternetExplorer() {
